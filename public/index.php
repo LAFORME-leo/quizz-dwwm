f6e906fade4bd4518c5b969179f362a747bd81a7
Leo <leo@PORT-21B-118>
2024-04-04 13:25:59 +0200

    push

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+require_once dirname(_DIR_) .'/vendor/autoload.php';
+echo "salut";
+var_dump($_SERVER);
+var_dump($_GET);
---
+++ src/model/Quizz.php
@@ -0,0 +1,7 @@
+<?php
+namespace app\Quizz\model;
+
+class Quizz
+{
+
+}
